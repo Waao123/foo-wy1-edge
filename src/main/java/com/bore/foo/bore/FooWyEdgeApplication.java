3570d62ac7889495c02b2975217f704f460e316c
wangyao <2511933030@qq.com>
2023-01-29 13:45:57 +0800

    init

---
+++ src/main/java/com/bore/foo/bore/FooWyEdgeApplication.java
@@ -0,0 +1,13 @@
+package com.bore.foo.bore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooWyEdgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FooWyEdgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bore/foo/bore/FooWyEdgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bore.foo.bore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FooWyEdgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
